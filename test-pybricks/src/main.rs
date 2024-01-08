a31dcd208612464503c1e0fb7df766408f3a5585
Johannes Ringler <johannesringler@gmail.com>
2024-01-08 23:15:14 +0100

    can consistently find city hub with name now

--- test-pybricks/src/main.rs
+++ test-pybricks/src/main.rs
@@ -1,5 +1,5 @@
 use btleplug::api::bleuuid::BleUuid;
-use btleplug::api::{Central, CentralEvent, Manager as _, Peripheral, ScanFilter};
+use btleplug::api::{Central, CentralEvent, Manager as _, Peripheral, ScanFilter, Service};
 use btleplug::platform::Manager;
 use futures::StreamExt;
 use uuid::Uuid;
@@ -15,34 +15,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let adapter = adapter_list.first().unwrap();
     // pybricks uuid: 'c5f50001-8280-46da-89f4-6d8051e4aeef'
     let pybricks_service_uuid = Uuid::from_u128(0xc5f50001828046da89f46d8051e4aeef);
-    let filter = ScanFilter {
-        services: vec![pybricks_service_uuid],
-    };
+    let filter = ScanFilter { services: vec![] };
 
-    let mut events = adapter.events().await?;
     adapter.start_scan(filter).await?;
     let mut device = None;
-    while let Some(event) = events.next().await {
-        match event {
-            CentralEvent::DeviceDiscovered(id) => {
-                println!("DeviceDiscovered: {:?}", id);
-                loop {
-                    // wait 1 second for name??
-                    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-                    let device_candidate = adapter.peripheral(&id).await?;
-                    let name = device_candidate.properties().await?.unwrap().local_name;
-                    println!("Device name: {:?}", name);
-                    if name != None {
-                        device = Some(device_candidate);
-                        break;
-                    }
-                }
-                adapter.stop_scan().await?;
-                break;
-            }
-            _ => {}
+    tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+    for device_candidate in adapter.peripherals().await? {
+        let properties = device_candidate.properties().await?.unwrap();
+        let name = properties.local_name;
+        let services = properties.services;
+        println!("Name: {:?} Services: {:?}", name, services);
+        if services.contains(&pybricks_service_uuid) {
+            println!("Found Pybricks service with device {:?}", name);
+            device = Some(device_candidate);
+            break;
         }
     }
+
+    adapter.stop_scan().await?;
     println!(
         "Connecting to {:?}",
         device.unwrap().properties().await?.unwrap().local_name
