62d21ff22d4bf276262c2e0d03658b120cdc55f4
Johannes Ringler <johannesringler@gmail.com>
2024-01-17 18:36:06 +0100

    enable env filter for logging

--- test-pybricks/src/main.rs
+++ test-pybricks/src/main.rs
@@ -4,11 +4,14 @@ use pybricks_ble::{
     io_hub::{IOHub, Input},
     pybricks_hub::BLEAdapter,
 };
+use tracing::debug;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
+        .with_env_filter(EnvFilter::from_default_env())
+        .compact()
         .init();
     let path = Path::new("../pybricks/programs/mpy/test_io.mpy");
 
@@ -20,9 +23,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     hub.discover(&adapter).await?;
     hub.connect().await?;
     tokio::time::sleep(std::time::Duration::from_secs(0)).await;
-    println!("cwd: {:?}", std::env::current_dir()?);
-
-    println!("path: {:?}", path);
     hub.download_program(&path).await?;
     hub.start_program().await?;
     tokio::time::sleep(std::time::Duration::from_secs(3)).await;
