fbd5ef4d63314cdd515fb85bc33cd6e3caa362a1
Johannes Ringler <johannesringler@gmail.com>
2024-03-23 09:43:36 +0100

    fix distance to segment func

--- brickrail/src/utils.rs
+++ brickrail/src/utils.rs
@@ -34,10 +34,10 @@ pub fn bresenham_line(start: (i32, i32), stop: (i32, i32)) -> Vec<(i32, i32)> {
 }
 
 // wikipedia
-pub fn distance_to_segment(p0: Vec2, p1: Vec2, p2: Vec2) -> f32 {
-    let t = (p0 - p1).dot(p2 - p1) / (p2 - p1).length();
+pub fn distance_to_segment(pos: Vec2, p1: Vec2, p2: Vec2) -> f32 {
+    let t = (pos - p1).dot(p2 - p1) / (p2 - p1).length_squared();
     let t = t.clamp(0.0, 1.0);
-    (p1 + (p2 - p1) * t - p0).length()
+    (p1 + (p2 - p1) * t - pos).length()
 
     // ((p2.x - p1.x) * (p1.y - p0.y) - (p1.x - p0.x) * (p2.y - p1.y)).abs()
     //    / ((p2.x - p1.x).powi(2) + (p2.y - p1.y).powi(2)).sqrt()
